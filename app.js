4e91cd41988b4cdf8865d0183b7e8bbe64b6fd4f
Katherine Day <kmday125@gmail.com>
2024-09-12 22:36:47 +0100

    cookie clicker button counts up by one and displays in browser

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 console.log("test");
 
+let cookieCount = 0;
+cookieCountContainer = document.getElementById("cookies-count");
+let cookiesPerSecond = 0;
+cookiesPerSecondContainer = document.getElementById("cookies-per-second");
+
+let shopUpgrades = [];
+
 async function getUpgrades() {
   const response = await fetch(
     "https://cookie-upgrade-api.vercel.app/api/upgrades"
@@ -7,8 +14,18 @@ async function getUpgrades() {
   console.log(response);
   const data = await response.json();
   console.log(data);
-  //const wrangledData = data.data[0];
+  //const wrangledData = data[0];
   //return wrangledData;
+  shopUpgrades.push(...data); //reddit and stackoverflow
 }
 
 getUpgrades();
+
+const myCookieButton = document.getElementById("cookie-button");
+
+function handleCounting() {
+  cookieCount = cookieCount + 1;
+  console.log(cookieCount);
+  cookieCountContainer.textContent = `Cookies Count: ${cookieCount}`;
+}
+myCookieButton.addEventListener("click", handleCounting);
