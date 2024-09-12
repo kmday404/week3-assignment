ac7069850d57998706825e5a0e1ba6611813393f
Katherine Day <kmday125@gmail.com>
2024-09-12 16:30:48 +0100

    fetched data from the API

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 console.log("test");
+
+async function getUpgrades() {
+  const response = await fetch(
+    "https://cookie-upgrade-api.vercel.app/api/upgrades"
+  );
+  console.log(response);
+  const data = await response.json();
+  console.log(data);
+  //const wrangledData = data.data[0];
+  //return wrangledData;
+}
+
+getUpgrades();
